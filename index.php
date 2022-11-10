97b5aa5170e631afd0343714e6185820e39e2601
bassp69 <mbintangassp@gmail.com>
2022-11-10 22:33:35 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Multimedia</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style2.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+</head>
+
+<body style="font-family: Monserat;">
+  <header>
+    <!-- As a link -->
+    <nav class="navbar  navbar-expand-lg bg-none">
+      <div class="container-fluid">
+        <a class="nav-link" href="#">
+          <h1 class="">KELOMPOK 5</h1>
+        </a>
+        <button class="navbar-toggler btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+          <i class="bi bi-window-stack"></i>
+        </button>
+
+
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Event</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Tips n Trick</a>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+      <div class="offcanvas-header">
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#aboutUs">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Event</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Tips n Trick</a>
+          </li>
+
+        </ul>
+
+      </div>
+  </header>
+  <main>
+
+    <section style="background-color: #0077C0;">
+
+      <div class="container-fluid">
+
+        <div class="row">
+          <div class="col-md-4 ms-auto mt-5 mb-5">
+            <h1 style="font-size: 3rem;">DO SOME MAGIC TIPS HEALTHY FOR
+              YOUTH STUDENT</h1>
+            <p>Mari jaga kesehatan mulai dari belajar merawat, memperhatikan dan memahami</p>
+          </div>
+          <div class="col-md-5 mb-5 ">
+            <div class="text-center mt-5">
+              <img src="https://mahasiswa.unair.ac.id/foto_mhs/152111513003.JPG" class="rounded-circle p-1" width="250rem" height="260rem">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section style="background-color: #0077C0;margin-top:10rem;">
+      <div class="text-center">
+        <h3>Tips and Trick</h3>
+      </div>
+      <div class="text-center">
+        <h2>How To Be Productive?</h2>
+      </div>
+      <div class="row mt-5">
+        <div class="col-md-3">
+          <div class="text-center">
+            <h1>hallo</h1>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="text-center">
+            <h1>hallo</h1>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="text-center">
+            <h1>hallo</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </main>
+  <footer>
+    <!-- place footer here -->
+  </footer>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
